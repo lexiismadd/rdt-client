5f152e297907df4afa08e8efed94315a6cba6602
laster13 <laster13012@gmail.com>
2024-03-23 18:35:33 +0000

    y

--- server/RdtClient.Service/Services/TorrentRunner.cs
+++ server/RdtClient.Service/Services/TorrentRunner.cs
@@ -911,7 +911,7 @@ public class TvMazeExternals
 }
 
 
-public string ExtractSeriesNameFromRdName(string rdName)
+private string ExtractSeriesNameFromRdName(string rdName, string category)
 {
     if (string.IsNullOrWhiteSpace(rdName))
     {
