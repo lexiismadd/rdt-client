1f3a77d13b78fe707b369aa2df9af354331aef01
laster13 <laster13012@gmail.com>
2024-03-24 16:57:55 +0000

    y

--- server/RdtClient.Service/Services/TorrentRunner.cs
+++ server/RdtClient.Service/Services/TorrentRunner.cs
@@ -910,8 +910,85 @@ public class TvMazeExternals
     public string TheTvdb { get; set; }
 }
 
+public string ExtractSeriesNameFromRdName(string rdName, string category)
+{
+    string seriesName = ExtractSeriesNameFromDecoupage(rdName);
 
-private string ExtractSeriesNameFromRdName(string rdName, string category)
+    if (string.IsNullOrEmpty(seriesName))
+    {
+        seriesName = ExtractSeriesNameFromRdNumber(rdName);
+
+        if (string.IsNullOrEmpty(seriesName))
+        {
+            seriesName = ExtractSeriesNameFromRdYear(rdName);
+        }
+    }
+
+    return seriesName;
+}
+
+private string ExtractSeriesNameFromDecoupage(string rdName)
+{
+    if (string.IsNullOrWhiteSpace(rdName))
+    {
+        return null;
+    }
+
+    // Remplacer les points par des espaces
+    rdName = rdName.Replace(".", " ");
+
+    // Recherche de la première occurrence d'un crochet fermant
+    int lastBracketIndex = rdName.LastIndexOf(']');
+
+    // Déterminer l'indice de début pour extraire le titre
+    int startIndex = lastBracketIndex == -1 ? 0 : lastBracketIndex + 1; // Commencer après le dernier crochet
+
+    // Rechercher le premier chiffre ou 'S' après le crochet fermant
+    int endIndex = startIndex;
+    while (endIndex < rdName.Length && !char.IsDigit(rdName[endIndex]) && rdName[endIndex] != 'S')
+    {
+        endIndex++;
+    }
+
+    string seriesName = rdName.Substring(startIndex, endIndex - startIndex).Trim();
+
+    return seriesName;
+}
+
+private string ExtractSeriesNameFromRdNumber(string rdName)
+{
+    if (string.IsNullOrWhiteSpace(rdName))
+    {
+        _logger.LogError("Le nom du fichier est vide ou null.");
+        return null;
+    }
+
+    _logger.LogInformation($"Nom du fichier : {rdName}");
+
+    // Remplacer les points par des espaces
+    rdName = rdName.Replace(".", " ");
+    _logger.LogInformation($"Nom du fichier après remplacement des points : {rdName}");
+
+    // Utiliser une expression régulière pour extraire le numéro suivi du titre
+    Regex regex = new Regex(@"(\d+)\s*(\S+)");
+    Match match = regex.Match(rdName);
+
+    string seriesName = null;
+
+    if (match.Success)
+    {
+        seriesName = match.Groups[1].Value + " " + match.Groups[2].Value;
+        _logger.LogInformation($"Série extraite : \"{seriesName}\"");
+    }
+    else
+    {
+        _logger.LogError("Aucune série trouvée dans le nom de fichier.");
+    }
+
+    return seriesName;
+}
+
+private string ExtractSeriesNameFromRdYear(string rdName)
 {
     if (string.IsNullOrWhiteSpace(rdName))
     {
