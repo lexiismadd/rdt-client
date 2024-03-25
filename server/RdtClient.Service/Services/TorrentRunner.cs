50847f528ee25ef910aa29e843b579d0a27c6cc1
laster13 <laster13012@gmail.com>
2024-03-25 16:36:24 +0000

    y

--- server/RdtClient.Service/Services/TorrentRunner.cs
+++ server/RdtClient.Service/Services/TorrentRunner.cs
@@ -597,6 +597,22 @@ public class TorrentRunner
                             Log($"Numero ID TMDB : {theTvdbId }");
                             //await AddMovieToRadarr(theTvdbId, seriesName, host, apiKey);
 
+// Appel de la méthode GetHostAndApiKeyFromConfig et récupération de l'host et de l'apiKey
+(string host, string apiKey) = await GetHostAndApiKeyFromConfig(torrent.Category, Settings.Get.General.RadarrSonarrInstanceConfigPath);
+
+// Vérification si l'host et l'apiKey ne sont pas null
+if (!string.IsNullOrEmpty(host) && !string.IsNullOrEmpty(apiKey))
+{
+    // Faites quelque chose avec l'host et l'apiKey
+    Console.WriteLine($"Host: {host}, ApiKey: {apiKey}");
+}
+else
+{
+    // Traitez le cas où l'host ou l'apiKey est null ou vide
+    Console.WriteLine("L'host ou l'apiKey est null ou vide.");
+}
+
+
 
 
                         // Ajouter un message de débogage pour indiquer que rien ne se passe pour la catégorie "radarr"
