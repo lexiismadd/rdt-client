1f9d0d9057cb669ed34f9ffbd2cf2088d23ff58a
laster13 <laster13012@gmail.com>
2024-03-26 18:41:27 +0000

    y

--- server/RdtClient.Service/Services/TorrentRunner.cs
+++ server/RdtClient.Service/Services/TorrentRunner.cs
@@ -578,7 +578,7 @@ public class TorrentRunner
 
                         if (torrent.Category.ToLower() == "sonarr")
                         {
-                            string seriesName = ExtractSeriesNameFromRdName(torrent.RdName, torrent.Category);
+                            //string seriesName = ExtractSeriesNameFromRdName(torrent.RdName, torrent.Category);
                             //Log($"Nom de la série (Sonarr) : {seriesName}");
                             // int? seriesId = await GetSeriesIdFromNameAsync(seriesName, torrent.Category, Settings.Get.General.RadarrSonarrInstanceConfigPath);
                             //int? theTvdbId = null;
@@ -588,14 +588,14 @@ public class TorrentRunner
                         }
                         else if (torrent.Category.ToLower() == "radarr")
                         {
-                            string seriesName = ExtractSeriesNameFromRdName(torrent.RdName, torrent.Category);
-                            Log($"Nom du Films (Radarr) : {seriesName}");
+                            //string seriesName = ExtractSeriesNameFromRdName(torrent.RdName, torrent.Category);
+                            //Log($"Nom du Films (Radarr) : {seriesName}");
                             // int? seriesId = await GetSeriesIdFromNameAsync(seriesName, torrent.Category, Settings.Get.General.RadarrSonarrInstanceConfigPath);
                             // int? theTvdbId = null;
                             // theTvdbId = await GetSeriesIdFromNameAsync(seriesName, torrent.Category);
                             // Log($"Numero ID TMDB : {theTvdbId }");
                             // await AddMovieToRadarr(theTvdbId, seriesName, torrent.Category, Settings.Get.General.RadarrSonarrInstanceConfigPath);
-                            await GetSeriesIdFromNameAsync(seriesName, torrent.Category, Settings.Get.General.RadarrSonarrInstanceConfigPath);
+                            int? seriesId = await GetSeriesIdFromNameAsync(seriesName, torrent.Category, Settings.Get.General.RadarrSonarrInstanceConfigPath);
 
 
                         }
