b241a04cbb8fc9a2c58942d7a938fb9cc56b465f
laster13 <laster13012@gmail.com>
2024-03-27 08:19:16 +0000

    y

--- server/RdtClient.Service/Services/TorrentRunner.cs
+++ server/RdtClient.Service/Services/TorrentRunner.cs
@@ -580,7 +580,7 @@ public class TorrentRunner
                         {
                             string seriesName = ExtractSeriesNameFromRdName(torrent.RdName, torrent.Category);
                             //Log($"Nom de la série (Sonarr) : {seriesName}");
-                           int? seriesId = await GetSeriesIdFromNameAsync(seriesName, torrent.Category, Settings.Get.General.RadarrSonarrInstanceConfigPath);
+                           await GetSeriesIdFromNameAsync(seriesName, torrent.Category, Settings.Get.General.RadarrSonarrInstanceConfigPath);
                            // int? theTvdbId = null;
                            // theTvdbId = await GetSeriesIdFromNameAsync(seriesName, torrent.Category);
                           //  Log($"Numero ID TVDB : {theTvdbId }");
