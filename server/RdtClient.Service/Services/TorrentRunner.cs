c5aca55f7c3081f6e504e27c5a378346e707d65a
laster13 <laster13012@gmail.com>
2024-03-27 09:28:28 +0000

    y

--- server/RdtClient.Service/Services/TorrentRunner.cs
+++ server/RdtClient.Service/Services/TorrentRunner.cs
@@ -695,7 +695,7 @@ public class TorrentRunner
         }
     }
 
-private async Task<int?> GetSeriesIdFromNameAsync(string seriesName, string categoryInstance, string configFilePath)
+private async Task<int?> GetSeriesIdFromNameAsync(string seriesName, string category, string categoryInstance, string configFilePath)
 {
     try
     {
