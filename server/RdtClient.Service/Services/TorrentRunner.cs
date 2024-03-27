b66996bbd18e8c4567bddfb9846018a15f798f0a
laster13 <laster13012@gmail.com>
2024-03-27 10:38:35 +0000

    Y

--- server/RdtClient.Service/Services/TorrentRunner.cs
+++ server/RdtClient.Service/Services/TorrentRunner.cs
@@ -697,7 +697,7 @@ private async Task<int?> GetMovieIdFromNameAsync(string seriesName, string categ
         }
         _logger.LogDebug($"numero TMDB : {apiConfig.Value.TmdbApi}");
 
-        string searchUrl = $"https://api.themoviedb.org/3/search/movie?api_key=apiConfig.Value.TmdbApi&query={HttpUtility.UrlEncode(seriesName)}";
+        string searchUrl = $"https://api.themoviedb.org/3/search/movie?{apiConfig.Value.TmdbApi}&query={HttpUtility.UrlEncode(seriesName)}";
 
         using (HttpClient httpClient = new HttpClient())
         {
